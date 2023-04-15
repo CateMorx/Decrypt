5ceb2bfbb5cae3ba5739b359e5ebea42561cfba0
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-04-15 17:49:09 +0800

    First commit

---
+++ Decrypt.py
@@ -0,0 +1,7 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 2 (Item 2:Decryption)
+
+#Imports necessary elements
+#Asks for user input
+#Converts the following characters: * = 'a', & = 'e' , # = 'i' , + = 'o',! = 'u'
+#Prints and animates the final output
